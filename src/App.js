d7e761e9d050505888abc420e4dfbf698699dc39
Lukasz Stefaniszyn <stefaniszynlukasz@gmail.com>
2019-07-16 08:47:34 +0200

    initial layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { steps } from './components/steps';
+import MultiStep from './components/MultiStep';
 
-function App() {
+export function App({ initialData }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Certified Continous Integration (CCI) wizard</h1>
+      <div>
+        <MultiStep showNavigation={true} steps={steps} />
+      </div>
     </div>
   );
 }
